31baac67e3e4f62aceb14c3ec0b3fc9ef92a2f1f
Steven Boutcher <boutchersj@gmail.com>
2024-11-29 20:37:13 -0600

    Try to fix favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-// @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://cosmicbobsleigh.com'
+});
