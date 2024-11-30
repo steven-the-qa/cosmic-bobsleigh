bf44805885fea71997c685741f1fc6b9fd7429ec
Steven Boutcher <boutchersj@gmail.com>
2024-11-29 21:15:46 -0600

    Update all the lighthouse experience cards

--- src/data/collections.ts
+++ src/data/collections.ts
@@ -7,13 +7,75 @@ export const collections: Collection[] = [
         description: 'Our journey to see every lighthouse in Door County',
         experiences: [
             {
-                id: 'cana-island',
-                title: 'Cana Island Lighthouse',
-                description: 'Historic lighthouse built in 1869, located on Lake Michigan',
-                imageUrl: '/images/cana-island.jpg',
+                id: 'baileys-harbor-range-1',
+                title: 'Baileys Harbor Range Light 1',
+                description: 'Visit the beautiful Baileys Harbor Range Light 1',
+                imageUrl: '/lighthouses/baileys_harbor_range_1.png',
                 date: '2023-06-15'
             },
-            // Add more lighthouses...
+            {
+                id: 'baileys-harbor-range-2',
+                title: 'Baileys Harbor Range Light 2',
+                description: 'Visit the beautiful Baileys Harbor Range Light 2',
+                imageUrl: '/lighthouses/baileys_harbor_range_2.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'birdcage',
+                title: 'Birdcage Lighthouse',
+                description: 'Visit the beautiful Birdcage lighthouse',
+                imageUrl: '/lighthouses/birdcage.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'cana-island',
+                title: 'Cana Island Lighthouse',
+                description: 'Visit the beautiful Cana Island lighthouse',
+                imageUrl: '/lighthouses/cana_island.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'eagle-bluff',
+                title: 'Eagle Bluff Lighthouse',
+                description: 'Visit the beautiful Eagle Bluff lighthouse',
+                imageUrl: '/lighthouses/eagle_bluff.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'kenosha-north-pier',
+                title: 'Kenosha North Pier Lighthouse',
+                description: 'Visit the beautiful Kenosha North Pier lighthouse',
+                imageUrl: '/lighthouses/kenosha_north_pier.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'kimberly-point',
+                title: 'Kimberly Point Lighthouse',
+                description: 'Visit the beautiful Kimberly Point lighthouse',
+                imageUrl: '/lighthouses/kimberly_point.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'lobster-point',
+                title: 'Lobster Point Lighthouse',
+                description: 'Visit the beautiful Lobster Point lighthouse',
+                imageUrl: '/lighthouses/lobster_point.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'plum-island',
+                title: 'Plum Island Lighthouse',
+                description: 'Visit the beautiful Plum Island lighthouse',
+                imageUrl: '/lighthouses/plum_island.png',
+                date: '2023-06-15'
+            },
+            {
+                id: 'sturgeon-bay-ship-canal',
+                title: 'Sturgeon Bay Ship Canal Lighthouse',
+                description: 'Visit the beautiful Sturgeon Bay Ship Canal lighthouse',
+                imageUrl: '/lighthouses/sturgen_bay_ship_canal.png',
+                date: '2023-06-15'
+            }
         ]
     },
     {
