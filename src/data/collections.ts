f03e1628fb127cd0e75c81e3cfb0b92aafb066a2
Steven Boutcher <boutchersj@gmail.com>
2024-12-01 14:20:36 -0600

    Order all experiences by date

--- src/data/collections.ts
+++ src/data/collections.ts
@@ -7,41 +7,6 @@ export const collections: Collection[] = [
         description: 'Our journey to see every lighthouse in Door County and beyond',
         experiences: [
             {
-                id: 'baileys-harbor-range-1',
-                title: 'Baileys Harbor Range Light 1',
-                description: 'The first of the 2 range lights, near the entrance to the Ridges Sanctuary. It\'s so tiny!',
-                imageUrl: '/lighthouses/baileys_harbor_range_1.png',
-                date: 'August 31, 2024'
-            },
-            {
-                id: 'baileys-harbor-range-2',
-                title: 'Baileys Harbor Range Light 2',
-                description: 'This one is much bigger! It\'s the house where the lighthouse keeper lived. We got to view the other range light using an old telescope in the attic.',
-                imageUrl: '/lighthouses/baileys_harbor_range_2.png',
-                date: 'August 31, 2024'
-            },
-            {
-                id: 'birdcage',
-                title: 'Birdcage Lighthouse',
-                description: 'Only accessible by boat, but we got to see it on a Bailey\'s Harbor cruise 🛥️',
-                imageUrl: '/lighthouses/birdcage.png',
-                date: 'September 8, 2024'
-            },
-            {
-                id: 'cana-island',
-                title: 'Cana Island Lighthouse',
-                description: 'We climbed the stairs to the top of this one! Such a beautiful view ❤️',
-                imageUrl: '/lighthouses/cana_island.png',
-                date: 'August 31, 2024'
-            },
-            {
-                id: 'eagle-bluff',
-                title: 'Eagle Bluff Lighthouse',
-                description: 'A must-see while we were at Peninsula State Park',
-                imageUrl: '/lighthouses/eagle_bluff.png',
-                date: 'July 14, 2024'
-            },
-            {
                 id: 'kenosha-north-pier',
                 title: 'Kenosha North Pier Lighthouse',
                 description: 'The first lighthouse we ever saw together ❤️',
@@ -49,25 +14,11 @@ export const collections: Collection[] = [
                 date: 'May 26, 2019'
             },
             {
-                id: 'kimberly-point',
-                title: 'Kimberly Point Lighthouse',
-                description: 'We found out the daughter of Kimberly-Clark\'s cofounder gave this lighthouse to the city of Neenah in 1945!',
-                imageUrl: '/lighthouses/kimberly_point.png',
-                date: 'October 25, 2024'
-            },
-            {
-                id: 'lobster-point',
-                title: 'Lobster Point Lighthouse',
-                description: 'The tiniest little lighthouse in Ogunquit, at the end of a half-mile footpath overlooking the Atlantic Ocean. Barely made it with my knee flaring up.',
-                imageUrl: '/lighthouses/lobster_point.png',
-                date: 'September 22, 2024'
-            },
-            {
-                id: 'plum-island',
-                title: 'Plum Island Lighthouse',
-                description: 'Spotted on the ferry ride to Washington Island',
-                imageUrl: '/lighthouses/plum_island.png',
-                date: 'October 5, 2024'
+                id: 'eagle-bluff',
+                title: 'Eagle Bluff Lighthouse',
+                description: 'A must-see while we were at Peninsula State Park',
+                imageUrl: '/lighthouses/eagle_bluff.png',
+                date: 'July 14, 2024'
             },
             {
                 id: 'sturgeon-bay-ship-canal',
@@ -84,11 +35,60 @@ export const collections: Collection[] = [
                 date: 'July 4, 2024'
             },
             {
+                id: 'baileys-harbor-range-1',
+                title: 'Baileys Harbor Range Light 1',
+                description: 'The first of the 2 range lights, near the entrance to the Ridges Sanctuary. It\'s so tiny!',
+                imageUrl: '/lighthouses/baileys_harbor_range_1.png',
+                date: 'August 31, 2024'
+            },
+            {
+                id: 'baileys-harbor-range-2',
+                title: 'Baileys Harbor Range Light 2',
+                description: 'This one is much bigger! It\'s the house where the lighthouse keeper lived. We got to view the other range light using an old telescope in the attic.',
+                imageUrl: '/lighthouses/baileys_harbor_range_2.png',
+                date: 'August 31, 2024'
+            },
+            {
+                id: 'cana-island',
+                title: 'Cana Island Lighthouse',
+                description: 'We climbed the stairs to the top of this one! Such a beautiful view ❤️',
+                imageUrl: '/lighthouses/cana_island.png',
+                date: 'August 31, 2024'
+            },
+            {
+                id: 'birdcage',
+                title: 'Birdcage Lighthouse',
+                description: 'Only accessible by boat, but we got to see it on a Bailey\'s Harbor cruise 🛥️',
+                imageUrl: '/lighthouses/birdcage.png',
+                date: 'September 8, 2024'
+            },
+            {
+                id: 'lobster-point',
+                title: 'Lobster Point Lighthouse',
+                description: 'The tiniest little lighthouse in Ogunquit, at the end of a half-mile footpath overlooking the Atlantic Ocean. Barely made it with my knee flaring up.',
+                imageUrl: '/lighthouses/lobster_point.png',
+                date: 'September 22, 2024'
+            },
+            {
                 id: 'winter-island',
                 title: 'Winter Island Lighthouse',
                 description: 'Surprise lighthouse visit on our trolley tour around Salem, Massachusetts',
                 imageUrl: '/lighthouses/winter_island.png',
                 date: 'September 22, 2024'
+            },
+            {
+                id: 'plum-island',
+                title: 'Plum Island Lighthouse',
+                description: 'Spotted on the ferry ride to Washington Island',
+                imageUrl: '/lighthouses/plum_island.png',
+                date: 'October 5, 2024'
+            },
+            {
+                id: 'kimberly-point',
+                title: 'Kimberly Point Lighthouse',
+                description: 'We found out the daughter of Kimberly-Clark\'s cofounder gave this lighthouse to the city of Neenah in 1945!',
+                imageUrl: '/lighthouses/kimberly_point.png',
+                date: 'October 25, 2024'
             }
         ]
     },
@@ -98,20 +98,6 @@ export const collections: Collection[] = [
         description: 'Our quest to dine at every Darden restaurant',
         experiences: [
             {
-                id: 'olive-garden',
-                title: 'Olive Garden',
-                description: 'No pic yet',
-                imageUrl: '/darden/olive_garden.png',
-                date: 'Date TBD'
-            },
-            {
-                id: 'longhorn-steakhouse',
-                title: 'LongHorn Steakhouse',
-                description: 'Dinner before we saw Wicked in 3D at the Marcus in Ashwaubenon',
-                imageUrl: '/darden/longhorn_steakhouse.png',
-                date: 'November 30, 2024'
-            },
-            {
                 id: 'cheddars',
                 title: 'Cheddar\'s Scratch Kitchen',
                 description: 'Lunch date when we saw the Kenosha North Pier Lighthouse',
@@ -119,25 +105,18 @@ export const collections: Collection[] = [
                 date: 'May 26, 2019'
             },
             {
-                id: 'yard-house',
-                title: 'Yard House',
-                description: 'Dinner in Seattle on our honeymoon ❤️',
-                imageUrl: '/darden/yard_house.png',
-                date: 'November 4, 2021'
-            },
-            {
-                id: 'seasons-52',
-                title: 'Seasons 52',
-                description: 'No pic yet',
-                imageUrl: '/darden/seasons_52.png',
-                date: 'Date TBD'
-            },
-            {
                 id: 'bahama-breeze',
                 title: 'Bahama Breeze',
                 description: 'Lunch date during our visit to Tampa',
                 imageUrl: '/darden/bahama_breeze.png',
                 date: 'October 3, 2019'
+            },
+            {
+                id: 'yard-house',
+                title: 'Yard House',
+                description: 'Dinner in Seattle on our honeymoon ❤️',
+                imageUrl: '/darden/yard_house.png',
+                date: 'November 4, 2021'
             },
             {
                 id: 'capital-grille',
@@ -152,6 +131,27 @@ export const collections: Collection[] = [
                 description: 'Birthday dinner ❤️ Right before she got a Cameo from Hidetoshi Imura ("Hide" from The Office)',
                 imageUrl: '/darden/eddie_v.png',
                 date: 'April 4, 2023'
+            },
+            {
+                id: 'longhorn-steakhouse',
+                title: 'LongHorn Steakhouse',
+                description: 'Dinner before we saw Wicked in 3D at the Marcus in Ashwaubenon',
+                imageUrl: '/darden/longhorn_steakhouse.png',
+                date: 'November 30, 2024'
+            },
+            {
+                id: 'olive-garden',
+                title: 'Olive Garden',
+                description: 'No pic yet',
+                imageUrl: '/darden/olive_garden.png',
+                date: 'Date TBD'
+            },
+            {
+                id: 'seasons-52',
+                title: 'Seasons 52',
+                description: 'No pic yet',
+                imageUrl: '/darden/seasons_52.png',
+                date: 'Date TBD'
             }
         ]
     }
