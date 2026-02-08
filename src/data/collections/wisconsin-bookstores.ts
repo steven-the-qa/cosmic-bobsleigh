fcaf2d2e1735268bb73e27b04bc54819d483584b
Steven Boutcher <boutchersj@gmail.com>
2026-02-07 20:26:01 -0600

    resolve rebase conflict

--- src/data/collections/wisconsin-bookstores.ts
+++ src/data/collections/wisconsin-bookstores.ts
@@ -8,37 +8,44 @@ export const wisconsinBookstores: Collection = {
         {
             id: 'novel-bay-booksellers',
             title: 'Novel Bay Booksellers',
-            description: '',
+            description: 'This is our local bookstore! It was the bookstore that made us want to see all the indie bookstores in Wisconsin. August 2025 was when we started buying books from indie bookstores, so that is the date I put here. We had visited this bookstore years before we moved to Sturgeon Bay when it looked quite different than it does today! (still need a pic of us in front of it)',
             imageUrl: '',
-            date: ''
+            date: 'August 12, 2025'
         },
         {
-            id: 'lions-mouth',
-            title: 'Lion\'s Mouth Bookstore',
-            description: '',
-            imageUrl: '',
-            date: 'January 24, 2026'
-        },
-        {
-            id: 'prairie-pages',
-            title: 'Prairie Pages',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'yardstick',
-            title: 'Yardstick Books',
-            description: '',
-            imageUrl: '',
-            date: ''
+            id: 'redbery-books',
+            title: 'Redbery Books',
+            description: 'We went here during our trip to the Apostle Islands! (still need a pic of us in front of it)',
+            imageUrl: '/wisconsin-bookstores/redbery_books.jpeg',
+            date: 'July 11, 2025'
         },
         {
             id: 'story-cellar',
             title: 'The Story Cellar',
-            description: '',
+            description: 'We went here on our trip to see Rib Mountain State Park! The basement has used books and a really cool speakeasy vibe. (still need a pic of us in front of it)',
             imageUrl: '',
-            date: ''
+            date: 'October 25, 2025'
+        },
+        {
+            id: 'yardstick',
+            title: 'Yardstick Books',
+            description: 'This one is over in Algoma, 20 minutes from us. I found a cool history book here that uses 47 national borders as a lens to talk about the history of various countries. (still need a pic of us in front of it)',
+            imageUrl: '/wisconsin-bookstores/yardstick.jpeg',
+            date: 'November 15, 2025'
+        },
+        {
+            id: 'prairie-pages',
+            title: 'Prairie Pages',
+            description: 'We came back to Sun Prairie a year and a half after moving away just to see this bookstore! It opened right next to a café and the two businesses promote each other ❤️ (still need a pic of us in front of it)',
+            imageUrl: '',
+            date: 'November 22, 2025'
+        },
+        {
+            id: 'lions-mouth',
+            title: 'Lion\'s Mouth Bookstore',
+            description: 'This one is in Green Bay! I bought my first Bernie Sanders book here, and they serve coffee drinks you can sip while you browse.',
+            imageUrl: '/wisconsin-bookstores/lions_mouth.jpeg',
+            date: 'January 24, 2026'
         },
         {
             id: 'fair-isle',
