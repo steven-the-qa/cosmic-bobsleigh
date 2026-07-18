0719f1bdec5c8f1c3f74978502fe25a0c9792abb
Steven Boutcher <boutchersj@gmail.com>
2026-07-17 20:34:47 -0500

    Add remaining Oregon lighthouses; add first Door County Land Trust Nature Preserve; add Aztalan State Park

--- src/data/collections/door-county-land-trust-nature-preserves.ts
+++ src/data/collections/door-county-land-trust-nature-preserves.ts
@@ -6,108 +6,108 @@ export const doorCountyLandTrustNaturePreserves: Collection = {
     description: 'Our quest to visit every Door County Land Trust nature preserve',
     experiences: [
         {
-            id: 'heins-creek',
-            title: 'Heins Creek Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
             id: 'bay-shore-blufflands',
             title: 'Bay Shore Blufflands Nature Preserve',
             description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'detroit-harbor',
-            title: 'Detroit Harbor Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'domer-neff',
-            title: 'Domer-Neff Nature Preserve and Bird Sanctuary',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'ephraim-anderson-pond',
-            title: 'Ephraim Nature Preserve at Anderson Pond',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'three-springs',
-            title: 'Three Springs Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'kangaroo-lake',
-            title: 'Kangaroo Lake Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'lautenbach-woods',
-            title: 'Lautenbach Woods Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'legacy-preserve-clay-banks',
-            title: 'Legacy Preserve at Clay Banks',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'little-lake',
-            title: 'Little Lake Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'oak-road',
-            title: 'Oak Road Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'richter-community-forest',
-            title: 'Richter Community Forest Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'sturgeon-bay-ship-canal',
-            title: 'Sturgeon Bay Ship Canal Nature Preserve',
-            description: '',
-            imageUrl: '',
-            date: ''
-        },
-        {
-            id: 'white-cliff',
-            title: 'White Cliff Nature Preserve',
-            description: '',
-            imageUrl: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/bay_shore_blufflands_nature_preserve.jpeg',
             date: ''
         },
         {
             id: 'bear-creek',
             title: 'Bear Creek Nature Preserve',
             description: '',
-            imageUrl: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/bear_creek_nature_preserve.jpeg',
+            date: 'July 5, 2026'
+        },
+        {
+            id: 'legacy-preserve-clay-banks',
+            title: 'Legacy Preserve at Clay Banks',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/legacy_preserve_at_clay_banks.jpeg',
+            date: 'July 5, 2026'
+        },
+        {
+            id: 'heins-creek',
+            title: 'Heins Creek Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/heins_creek_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'detroit-harbor',
+            title: 'Detroit Harbor Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/detroit_harbor_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'domer-neff',
+            title: 'Domer-Neff Nature Preserve and Bird Sanctuary',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/domer_neff_nature_preserve_and_bird_sanctuary.jpeg',
+            date: ''
+        },
+        {
+            id: 'ephraim-anderson-pond',
+            title: 'Ephraim Nature Preserve at Anderson Pond',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/ephraim_nature_preserve_at_anderson_pond.jpeg',
+            date: ''
+        },
+        {
+            id: 'three-springs',
+            title: 'Three Springs Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/three_springs_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'kangaroo-lake',
+            title: 'Kangaroo Lake Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/kangaroo_lake_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'lautenbach-woods',
+            title: 'Lautenbach Woods Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/lautenbach_woods_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'little-lake',
+            title: 'Little Lake Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/little_lake_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'oak-road',
+            title: 'Oak Road Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/oak_road_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'richter-community-forest',
+            title: 'Richter Community Forest Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/richter_community_forest_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'sturgeon-bay-ship-canal',
+            title: 'Sturgeon Bay Ship Canal Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/sturgeon_bay_ship_canal_nature_preserve.jpeg',
+            date: ''
+        },
+        {
+            id: 'white-cliff',
+            title: 'White Cliff Nature Preserve',
+            description: '',
+            imageUrl: '/door-county-land-trust-nature-preserves/white_cliff_nature_preserve.jpeg',
             date: ''
         },
     ]
--- src/data/collections/door-county.ts
+++ src/data/collections/door-county.ts
@@ -225,105 +225,105 @@ export const doorCounty: Collection = {
                     id: 'heins-creek',
                     title: 'Heins Creek Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/heins_creek_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'bay-shore-blufflands',
                     title: 'Bay Shore Blufflands Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/bay_shore_blufflands_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'detroit-harbor',
                     title: 'Detroit Harbor Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/detroit_harbor_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'domer-neff',
                     title: 'Domer-Neff Nature Preserve and Bird Sanctuary',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/domer_neff_nature_preserve_and_bird_sanctuary.jpeg',
                     date: ''
                 },
                 {
                     id: 'ephraim-anderson-pond',
                     title: 'Ephraim Nature Preserve at Anderson Pond',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/ephraim_nature_preserve_at_anderson_pond.jpeg',
                     date: ''
                 },
                 {
                     id: 'three-springs',
                     title: 'Three Springs Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/three_springs_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'kangaroo-lake',
                     title: 'Kangaroo Lake Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/kangaroo_lake_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'lautenbach-woods',
                     title: 'Lautenbach Woods Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/lautenbach_woods_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'legacy-preserve-clay-banks',
                     title: 'Legacy Preserve at Clay Banks',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/legacy_preserve_at_clay_banks.jpeg',
                     date: ''
                 },
                 {
                     id: 'little-lake',
                     title: 'Little Lake Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/little_lake_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'oak-road',
                     title: 'Oak Road Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/oak_road_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'richter-community-forest',
                     title: 'Richter Community Forest Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/richter_community_forest_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'sturgeon-bay-ship-canal',
                     title: 'Sturgeon Bay Ship Canal Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/sturgeon_bay_ship_canal_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'white-cliff',
                     title: 'White Cliff Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/white_cliff_nature_preserve.jpeg',
                     date: ''
                 },
                 {
                     id: 'bear-creek',
                     title: 'Bear Creek Nature Preserve',
                     description: '',
-                    imageUrl: '',
+                    imageUrl: '/door-county-land-trust-nature-preserves/bear_creek_nature_preserve.jpeg',
                     date: ''
                 },
             ]
