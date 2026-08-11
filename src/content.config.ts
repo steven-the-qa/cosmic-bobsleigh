ec673073d5817bfb40c5fb54d8565966b9e23233
Steven Boutcher <boutchersj@gmail.com>
2026-08-11 12:57:45 -0500

    Fix schema def

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,14 @@
-import { defineCollection, z } from 'astro:content';
-import { object, string } from 'astro:schema';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
     loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/data/blog" }),
-    schema: object({
-        title: string(),
-        date: string().optional(),
-        description: string().optional(),
-        experienceId: string()
+    schema: z.object({
+        title: z.string(),
+        date: z.string().optional(),
+        description: z.string().optional(),
+        experienceId: z.string()
     }),
 });
 
